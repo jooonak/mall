f86b86cd790dd9aa0f947c5a9a6e6a046d2dcccc
joonak <dlwnsgkr11_@naver.com>
2018-07-30 11:09:47 +0900

    remove scss files && add mailing logic

---
+++ src/main/java/com/worksout/controller/common/MailController.java
@@ -0,0 +1,29 @@
+package com.worksout.controller.common;
+
+import com.worksout.common.Mailer;
+import com.worksout.dto.member.PasswordResetToken;
+import com.worksout.service.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mail")
+public class MailController {
+
+    @Autowired
+    Mailer mailer;
+
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping("/password-token")
+    public String passwordToken(PasswordResetToken token, Model model) {
+        PasswordResetToken resetToken = memberService.getToken(token);
+        model.addAttribute("resetToken", resetToken);
+        return "common/password-token";
+    }
+
+}

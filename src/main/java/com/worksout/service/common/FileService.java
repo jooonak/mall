8efe1c1abd67eeb042d46281ec2e15cd2aa88657
Joonak <dlwnsgkr11_@naver.com>
2018-08-29 10:50:11 +0900

    add propertySource annotation in FileService

--- src/main/java/com/worksout/service/common/FileService.java
+++ src/main/java/com/worksout/service/common/FileService.java
@@ -4,12 +4,14 @@ import com.worksout.common.FTPWorker;
 import com.worksout.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @Service
+@PropertySource("classpath:/config/files.properties")
 public class FileService {
 
 	@Value("${ftp.image.path}")
